4f07c4b70a90ce487ab321871b9c20262fd82d6e
SK <55817746+SKCro@users.noreply.github.com>
2023-06-30 13:53:31 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# WebTV-CSS-Remake
+ A project that aims to recreate WebTV Networks' page stylization and stuff in modern CSS and HTML.
---
+++ js/main.js
@@ -0,0 +1,71 @@
+// If you're wondering, this project was originally built with the Tizen IDE.
+
+// I was going to make it a Samsung TV app, but decided to make it compatible
+// with other browsers as well. This JS is commented out because the IDE kept
+// complaining and I only need it for reference anyways.
+
+// Right now, this doesn't contain anything interesting - I might repurpose it
+// later on for actual page functionality and logic.
+
+// All this file is, right now, is just some example JS from the IDE, meant to
+// show off some sort of clock functionality.
+
+
+
+// var checkTime;
+
+//Initialize function
+// var init = function () {
+    // TODO:: Do your initialization job
+//    console.log('init() called');
+    
+//    document.addEventListener('visibilitychange', function() {
+//        if(document.hidden){
+//            // Something you want to do when hide or exit.
+//        } else {
+//            // Something you want to do when resume.
+//        }
+//    });
+ 
+    // add eventListener for keydown
+//    document.addEventListener('keydown', function(e) {
+//    	switch(e.keyCode){
+//    	case 37: //LEFT arrow
+//    		break;
+//    	case 38: //UP arrow
+//    		break;
+//    	case 39: //RIGHT arrow
+//    		break;
+//    	case 40: //DOWN arrow
+//    		break;
+//    	case 13: //OK button
+//    		break;
+//    	case 10009: //RETURN button
+//		tizen.application.getCurrentApplication().exit();
+//    		break;
+//    	default:
+//    		console.log('Key code : ' + e.keyCode);
+//    		break;
+//    	}
+//    });
+//};
+// window.onload can work without <body onload="">
+//window.onload = init;
+
+// function startTime() {
+//     var today = new Date();
+//    var h = today.getHours();
+//    var m = today.getMinutes();
+//    var s = today.getSeconds();
+//    m = checkTime(m);
+//    s = checkTime(s);
+//    document.getElementById('divbutton1').innerHTML='Current time: ' + h + ':' + m + ':' + s;
+//    setTimeout(startTime, 10);
+// }
+
+// function checkTime(i) {
+//    if (i < 10) {
+//        i='0' + i;
+//    }
+//    return i;
+// }
