3bfbad95670e8d7808eed31697f02e1e0574e298
home-github <124263428+home-gihub@users.noreply.github.com>
2024-11-16 09:35:31 +0000

    Update search.js
    Signed-off-by: home-github <124263428+home-gihub@users.noreply.github.com>

--- wtv-center/js/search.js
+++ wtv-center/js/search.js
@@ -2,6 +2,6 @@ function search(event) {
   event.preventDefault();
   setTimeout(function() {
     var query = document.getElementById('q').value;
-    window.open("https://theoldnet.com/get?url=" + query, '&year=1999&scripts=false&decode=false');
+    window.location = "https://theoldnet.com/get?url=" + query, '&year=1999&scripts=false&decode=false';
   }, 235);
 }
